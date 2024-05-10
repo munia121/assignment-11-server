73a6896aec1b69847d7dec8bbe6fb2ec9e81e2bf
unknown <muniyaakter21@gmail.com>
2024-05-10 21:34:50 +0600

    added post method for create data

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000
 
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6irp4bx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -25,12 +25,30 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
+        const database = client.db("BookLibrary");
+        const bookCollection = database.collection("Books");
+        const categoryCollection = database.collection("BookCategory");
+
+
+        // app.get('/books', async (req, res) => {
+        //     const cursor = bookCollection.find();
+        //     const result = await cursor.toArray();
+        //     res.send(result)
+        // })
+
+        app.post('/addBooks', async (req, res) => {
+            const book = req.body;
+            // console.log(book) 
+            const result = await bookCollection.insertOne(book);
+            res.send(result);
+            // console.log(result)
+        })
 
 
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
